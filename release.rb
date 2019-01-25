41773fba28bb614be8b0eb6615832d9a0d1ebf49
philearley <phillip.earley@sage.com>
2019-01-25 13:38:47 +0000

    Run release steps for v2_58_0

--- release.rb
+++ release.rb
@@ -22,4 +22,4 @@ EOF
 hub add .
 hub commit -m "$RELEASE_TITLE"
 hub push origin release-$VERSION
-hub pull-request -b "philearley:master" -h "philearley:release-$VERSION" -F "./release-file.txt" -A "philearley"
+hub pull-request -b "philearley:master" -h "philearley:release-$VERSION" -F "./release-file.txt" --assign "philearley"
