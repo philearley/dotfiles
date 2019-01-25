540d4dd3b1384d4310db557eaee019619c01a491
philearley <phillip.earley@sage.com>
2019-01-25 16:58:53 +0000

    Run release steps for v2_58_0

--- release.sh
+++ release.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-VERSION=$1
+VERSION=v2_58_0
 
-# while read p; do
-#   echo "$p"
-# done < ./release_notes/$VERSION.html.md
-
-release_notes=$(cat "./../s1_gac_ui/release_notes/$VERSION.html.md")
+release_notes=$(cat "v2_58_0.html.md")
 
 git checkout -b release-$VERSION
 
@@ -19,9 +15,8 @@ cat > "./release-file.txt" <<EOF
 EOF
 
 
-
-/home/developer/go/src/github.com/github/hub/bin/hub add .
-/home/developer/go/src/github.com/github/hub/bin/hub commit -m "$RELEASE_TITLE"
-/home/developer/go/src/github.com/github/hub/bin/hub push origin release-$VERSION
+hub add .
+hub commit -m "$RELEASE_TITLE"
+hub push origin release-$VERSION
 # hub pull-request -b "philearley:master" -h "philearley:release-$VERSION" -F "./release-file.txt" --assign "philearley"
-/home/developer/go/src/github.com/github/hub/bin/hub pull-request -a "philearley" -b "philearley:master" -h "philearley:release-$VERSION" -m "$RELEASE_TITLE" -m "" -m "$release_notes" -l "Targaryen,Pending Review"
+hub pull-request -a "philearley" -b "philearley:master" -h "philearley:release-$VERSION" -m "$RELEASE_TITLE" -m "" -m "$release_notes" -l "Targaryen,Pending Review"
